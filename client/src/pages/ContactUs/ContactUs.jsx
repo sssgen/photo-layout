7e64d67be171bd2b4606edb7e1b40ef5a2ebc7fb
cementix <bodakondrackij@gmail.com>
2023-09-08 16:54:33 +0300

    contact us page

---
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactUs.css';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <h1>pistalet</h1>
+        </div>
+    );
+};
+
+export default ContactUs;
