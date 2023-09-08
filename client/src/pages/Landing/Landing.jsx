a03423f0a8e8da3e95c1b84a788efc8fbc8b5a88
cementix <bodakondrackij@gmail.com>
2023-09-08 16:54:44 +0300

    landing page

---
+++ client/src/pages/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+    return (
+        <div>
+            <h1>Hello CHMO!</h1>
+        </div>
+    );
+};
+
+export default Landing;
