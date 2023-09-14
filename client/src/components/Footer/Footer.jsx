32158fe96e3f2d04d717aa3d2536ab1097b84319
sssGen <deskarte.des@gmail.com>
2023-09-14 22:34:18 +0300

    footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,25 +6,32 @@ import logo from '../../Images/logo.png'
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <div className={styles.footerMain}>
-                <img src={logo} alt='logo'/>
-            </div>
-            <div className={styles.footerLinks}>
-                <h3>For customer</h3>
-                <Link>Price</Link>
-                <Link>Reviews</Link>
-                <Link>Contacts</Link>
-                <Link>Delivery</Link>
-                <Link>Master</Link>
-            </div>
-            <div className={styles.footerLinks}>
-                <h3>Production</h3>
-                <Link>Services</Link>
-                <Link>Irregular pictures</Link>
-                <Link>Board games</Link>
-            </div>
-            <div className={styles.footerContact}>
-
+            <div className={styles.footerContainer}>
+                <div className={styles.footerMain}>
+                    <Link to='/'>
+                        <img className='logo' src={logo} alt='logo'/>
+                    </Link>
+                    <p> Kyiv, lorem ipsum 22-a </p>
+                    <p> kyivprint@gmail.com </p>
+                </div>
+                <div className={styles.footerLinks}>
+                    <h3>For customer</h3>
+                    <Link>Price</Link>
+                    <Link>Reviews</Link>
+                    <Link to='/contact'>Contacts</Link>
+                    <Link>Delivery</Link>
+                    <Link>Master</Link>
+                </div>
+                <div className={styles.footerLinks}>
+                    <h3>Production</h3>
+                    <Link>Services</Link>
+                    <Link>Irregular pictures</Link>
+                    <Link>Board games</Link>
+                </div>
+                <div className={styles.footerLinks}>
+                    <h3>Call us!</h3>
+                    <h4>+380 98 888 8888</h4>
+                </div>
             </div>
         </div>
     );
