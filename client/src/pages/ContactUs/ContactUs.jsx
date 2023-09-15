e736cdc60b7fec0c705b4360a37ac8680174aa2c
sssGen <deskarte.des@gmail.com>
2023-09-15 08:44:01 +0300

    contact us form

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactUs.scss';
+import MainButton from '../../components/UI/MainButton/MainButton';
+import phoneIcon from '../../images/phoneIcon.png';
+import emailIcon from '../../images/emailIcon.png';
+import placeIcon from '../../images/placeIcon.png';
 
 const ContactUs = () => {
+    
+    function submitForm(e){
+        e.preventDefault()
+        console.log(username, email)
+    }
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+
     return (
         <div className='contactWrapper'>
             <div className='contactAddress'>
-                city: Kyiv, Sherbak str., 27 
+                <h1>Contacts</h1>
+                <p><img src={placeIcon} alt='location'/>Kyiv, lorem ipsum 22-a</p>
+                <p><img src={phoneIcon} alt='phone number'/>+380 98 888 8888</p>
+                <p><img src={emailIcon} alt='email'/>kyivprint@gmail.com</p> 
             </div>
-            <div className='contactForm'>
-
-            </div>
+            <form className='contactForm' onSubmit={(e) => submitForm(e)}>
+                <input 
+                    type='text'
+                    required='required'
+                    placeholder='Username..'
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input
+                    type='email'
+                    required='required'
+                    placeholder='Email..'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <MainButton onClick={()=> submitForm() }>Send</MainButton>
+            </form>
         </div>
-    );
+    );  
 };
 
 export default ContactUs;
