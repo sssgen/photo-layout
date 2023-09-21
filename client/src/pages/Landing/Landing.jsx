76901dfb67dfd7bc9064521e09d8e05e9215bf05
Bohdan <bodakondrackij@gmail.com>
2023-09-21 19:34:08 +0300

    responsive "how it works?" and benefits block

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -3,8 +3,18 @@ import { Link } from 'react-router-dom';
 import MainButton from '../../components/UI/MainButton/MainButton';
 import bigFormat from '../../images/bigFormat.png';
 import canvaPhotos from '../../images/canvaPhotos.png';
+import comfortIcon from '../../images/comfortIcon.svg';
+import deliveryIcon from '../../images/deliveryIcon.svg';
+import downArrow from '../../images/downArrow.svg';
+import guaranteeIcon from '../../images/guaranteeIcon.svg';
+import iconReceive from '../../images/iconReceive.svg';
+import iconSelect from '../../images/iconSelect.svg';
+import iconUpload from '../../images/iconUpload.svg';
+import operativelyIcon from '../../images/operativelyIcon.svg';
+import payIcon from '../../images/payIcon.svg';
 import photoCards from '../../images/photoCards.png';
 import polaroidPhotos from '../../images/polaroidPhotos.png';
+import qualityIcon from '../../images/qualityIcon.svg';
 import squarePhotos from '../../images/squarePhotos.jpg';
 import './Landing.scss';
 
@@ -103,6 +113,77 @@ function Landing() {
                                 <button className='canvaPrice priceButton servicesButton'>Price</button>
                                 <button className='canvaOrder orderButton servicesButton'>Order</button>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='workProcedureWrapper'>
+                <div className='workProcedure'>
+                    <div className='howItWorksText'>
+                        <h3>How it works?</h3>
+                        <p>Brief and clear explanation of our work with clients</p>
+                        <img src={downArrow} alt='arrow down' />
+                    </div>
+                    <div className='workSteps'>
+                        <div className='selectStep workStep'>
+                            <img src={iconSelect} alt='icon select' />
+                            <div className='workStepText'>
+                                <h4>1. Select products</h4>
+                                <p>Select products you are interested in</p>
+                            </div>
+                        </div>
+                        <div className='uploadStep workStep'>
+                            <img src={iconUpload} alt='icon upload' />
+                            <div className='workStepText'>
+                                <h4>2. Upload photos</h4>
+                                <p>Upload the photos you need to print</p>
+                            </div>
+                        </div>
+                        <div className='receiveStep workStep'>
+                            <img src={iconReceive} alt='icon receive' />
+                            <div className='workStepText'>
+                                <h4>3. Receive it and only then pay</h4>
+                                <p>Pay for your order after receiving it</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='benefitsWrapper'>
+                <div className='benefits'>
+                    <h3>Benefits</h3>
+                    <div className='benefitsFlex'>
+                        <div className='comfortableBenefit benefit'>
+                            <img src={comfortIcon} alt='comfort logo' className='benefitIcon' />
+                            <h5 className='benefitHeading'>Comfort</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='qualityBenefit benefit'>
+                            <img src={qualityIcon} alt='quality logo' className='benefitIcon' />
+                            <h5 className='benefitHeading'>Quality</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='payBenefit benefit'>
+                            <img src={payIcon} alt='pay logo' className='benefitIcon' />
+                            <h5 className='benefitHeading'>Pay</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='operativelyBenefit benefit'>
+                            <img src={operativelyIcon} alt='operatively logo' className='benefitIcon' />
+                            <h5 className='benefitHeading'>Operatively</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='guaranteeBenefit benefit'>
+                            <img src={guaranteeIcon} alt='guarantee logo' className='benefitIcon' />
+                            <h5 className='benefitHeading'>Guarantee</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
+                        </div>
+                        <div className='deliveryBenefit benefit'>
+                            <img src={deliveryIcon} alt='delivery logo' className='benefitIcon' />
+                            <h5 className='benefitHeading' >Delivery</h5>
+                            <p className='benefitInfo'>Lorem ipsum dolor sit amet</p>
                         </div>
                     </div>
                 </div>
